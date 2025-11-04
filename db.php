5a3f47a562ae847b8586d110d58ddf4299f43e42
Unrandom0508 <juanalejandroperez0508@gmail.com>
2025-11-04 10:46:33 -0500

    deploy automatico y secretos

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost:mysql-flaw.alwaysdata.net", "flaw", "misifu123+", "flaw_inventario2");
+$conexion = new mysqli("localhost:https://github.com/Unrandom0508/inventario2.git", "flaw", "misifu123+", "flaw_inventario2");
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
